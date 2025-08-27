024f7a505268dcabe5cf170c41204dba2dbf900b
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-08-27 18:45:46 -0300

    group of middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Http\Middleware\Test1;
+use App\Http\Middleware\Test2;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->middleware(Test1::class);
+
+Route::middleware([Test1::class, Test2::class])->group(function () {
+    Route::get('/users', function () {
+        return ['users'];
+    });
+
+    Route::get('/profile', function () {
+        return 'profile';
+    });
+
+    Route::withoutMiddleware([Test1::class])->group(function () {
+        Route::get('/assinatura', function () {
+            return 'assinatura';
+        });
+
+        Route::get('/projeto', function () {
+            return 'projeto';
+        });
+    });
 });
+
